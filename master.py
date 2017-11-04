9a222368ff193ab74434b41e1a9dea0f19c83fa9
Raman Natarajan <harini.kottilingamprabhu@georgebrown.ca>
2017-11-04 00:34:21 -0400

    Modulized couontry.py

--- master.py
+++ master.py
@@ -1,3 +1,3 @@
 from modules.calculator import calculator_main
-calculator_main()
-
+from modules.country import callCountryBot
+callCountryBot()
--- modules/country.py
+++ modules/country.py
@@ -1,10 +1,12 @@
 import json
-from assets.countryHelper import countryHelper
+from modules.assets.countryHelper import countryHelper
 
-countryData = countryHelper.getCountryData()
-keyString, inputList = countryHelper.getInput()
+def getData():
+    countryData = countryHelper.getCountryData()
+    keyString, inputList = countryHelper.getInput()
+    return inputList, countryData
 
-def countryBot(inputList):
+def countryBot(inputList, countryData):
     if (len(inputList)==2):
         try:
             information = countryData[inputList[1]][inputList[0]]
@@ -12,11 +14,11 @@ def countryBot(inputList):
             decision = input("You want to continue learn about the numbers related to countries? Press Y or N:")
             if decision == "y" or decision == "Y":
                 keyString, inputList = countryHelper.getInput()
-                countryBot(inputList)
+                countryBot(inputList, countryData)
         except:
             print ("I don't think I know what you asked, please ask me something I know!")
             keyString, inputList = countryHelper.getInput()
-            countryBot(inputList)
+            countryBot(inputList, countryData)
 
     elif (len(inputList) == 1 ):
         count = 0
@@ -25,9 +27,15 @@ def countryBot(inputList):
                 if count <= 10:
                     print(countryData[a][inputList[0]])
                     count = count + 1
+            decision = input("You want to continue learn about the numbers related to countries? Press Y or N:")
+            if decision == "y" or decision == "Y":
+                keyString, inputList = countryHelper.getInput()
+                countryBot(inputList, countryData)
         except KeyError:
             print ("I don't think I know what you asked, please ask me something I know!")
             keyString, inputList = countryHelper.getInput()
-            countryBot(inputList)
-
-countryBot(inputList)
+            countryBot(inputList, countryData)
+def callCountryBot():
+    inputList, countryData = getData()
+    countryBot(inputList, countryData)
+callCountryBot()
